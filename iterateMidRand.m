42ed727fd80dc4d827146801c5601c0de12d0f9c
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-28 16:07:56 -0400

    implemented iterateMidRand

--- iterateMidRand.m
+++ iterateMidRand.m
@@ -1,8 +1,11 @@
-function [ output_args ] = iterateMidRand(num)
+function iterateMidRand(num,iter)
 %ITERATEMIDRAND same as IterateMid except this function produces its own
 %vectors
 %   produces vectors randomly using generateGaussian
 %   produces matrix M using produceMidPointMat()
 
+x = generateGaussians(1,num);
+y = generateGaussians(1,num);
+iterateMid(x',y',iter);
 
 end
